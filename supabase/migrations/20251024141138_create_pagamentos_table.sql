8dd392794bbd2523b8bb0fa93cff1b07a0a661ec
DevBeatrizAranha <bmja.dev@gmail.com>
2025-10-24 11:31:40 -0300

    corrigindo erro na query de tabela de pagamentos

--- supabase/migrations/20251024141138_create_pagamentos_table.sql
+++ supabase/migrations/20251024141138_create_pagamentos_table.sql
@@ -1,8 +1,8 @@
 -- Tabela de pagamentos
 CREATE TABLE pagamentos (
-  id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
-  pedido_id UUID NOT NULL REFERENCES pedidos(id) ON DELETE CASCADE,
+  id BIGSERIAL PRIMARY KEY,
+  pedido_id BIGINT NOT NULL REFERENCES pedidos(id) ON DELETE CASCADE,
   valor NUMERIC(10,2) NOT NULL,
-  status TEXT NOT NULL DEFAULT 'aprovado', -- sempre aprovado
+  status TEXT NOT NULL DEFAULT 'aprovado',
   criado_em TIMESTAMP WITH TIME ZONE DEFAULT NOW()
 );
